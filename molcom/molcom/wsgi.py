7612f1578372a34a8910859ca56961323adc1a58
Ojas Patel <ojas@ojas.net>
2013-12-19 14:51:44 -0600

    api

--- molcom/molcom/wsgi.py
+++ molcom/molcom/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append( os.path.abspath( os.path.join(os.path.dirname(__file__), '..') ) )
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "molcom.settings")
 
 from django.core.wsgi import get_wsgi_application
